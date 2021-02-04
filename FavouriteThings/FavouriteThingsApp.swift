2cd9bad2eaf1de9e398bc4f0732486f494e06b3a
Yeseo Kim <YKim@lcs.on.ca>
2021-02-04 09:48:09 -0500

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Yeseo Kim on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
